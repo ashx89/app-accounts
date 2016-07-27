83f76ff0f64bb80ab5a4f2f6986214968e39fb74
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-27 15:17:08 +0100

    reverted users to mongoose

--- components/users/fetch.js
+++ components/users/fetch.js
@@ -7,53 +7,23 @@ var Account = require(global.__accounts_base + '/models/account');
 var fetch = function onFetch(req, res, next) {
 	var id = req.params.id;
 
-	var resultsObject = {
-		users: []
-	};
-
+	var usersArray = [];
 	var userQuery = (id) ? { _id: id } : {};
 
-	User.paginate(userQuery, {
-		page: parseInt(req.query.page, 10) || 1,
-		limit: parseInt(req.query.limit, 10) || 10
-	}).then(function onPaginate(result) {
-		if (!result.docs.length) return next(new Error('Users not found'));
+	User.find(userQuery, function onUsersFind(err, users) {
+		if (err) return next(err);
+		if (!users.length) return next(new Error('Users not found'));
 
-		resultsObject.pagination = {
-			total: result.total,
-			limit: result.limit,
-			page: result.page,
-			pages: result.pages
-		};
-
-		result.docs.forEach(function onEachUser(user, index) {
+		users.forEach(function onEachUser(user, index) {
 			Account.find({ user: user._id }, function onFind(err, account) {
 				if (err) return next(err);
 
-				resultsObject.users.push({ user: user, account: account });
+				usersArray.push({ meta: user, account: account });
 
-				if (result.docs.length === index + 1) return res.status(200).json(resultsObject);
+				if (users.length === index + 1) return (usersArray.length === 1) ? res.status(200).json(usersArray[0]) : res.status(200).json(usersArray);
 			});
 		});
 	});
-
-	// User.find(userQuery)
-	// 	.limit(parseInt(req.query.limit, 10) || 10)
-	// 	.sort({ lastname: 1 })
-	// 	.exec(function onUsersFind(err, users) {
-	// 		if (err) return next(err);
-	// 		if (!users.length) return next(new Error('Users not found'));
-
-	// 		users.forEach(function onEachUser(user, index) {
-	// 			Account.find({ user: user._id }, function onFind(err, account) {
-	// 				if (err) return next(err);
-
-	// 				usersArray.push({ meta: user, account: account });
-
-	// 				if (users.length === index + 1) return (usersArray.length === 1) ? res.status(200).json(usersArray[0]) : res.status(200).json(usersArray);
-	// 			});
-	// 		});
-	// 	});
 };
 
 module.exports = fetch;
