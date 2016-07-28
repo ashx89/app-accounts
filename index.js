d99b236f349c1a15f9d2cb37411a1e49f2f7745e
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-28 18:39:52 +0100

    hidden route to single user [bug]

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/accounts/search', require('./components/accounts/search'));
  * Rest:: Users
  */
 app.get('/users/search', require('./components/users/search'));
-app.get('/users/:id', require('./components/users/fetch'));
+// app.get('/users/:id', require('./components/users/fetch'));
 
 
 module.exports = {
